0e7fc905c5d5b66da18146fab1bd44121fa20438
Clea Dronne <cleadronne@gmail.com>
2022-03-22 18:49:48 +0000

    js

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
-alert("thank you")
+document.getElementById("share_button").addEventListener("click", function(){
+    prompt("Who is your biggest female inspiration?");
+    alert("Thank you for your submission!");
+})
